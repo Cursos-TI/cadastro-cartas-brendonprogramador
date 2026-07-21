6f0a2c5c6bb231e375076f169d5faecedbdef489
brendonprogramador <brendon765122@gmail.com>
2026-07-21 20:21:26 +0000

    ADICIONANDO DP E PPC !

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,81 +6,106 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char estado1[10];
+char estado1;
 char codigo1[4];
+char cidade1[50];
 float populacao1;
 float area1;
 float pib1;
 int pontos_turisticos1;
+float densidade_populacional1;
+float pib_por_capita1;
 
-char estado2[10];
+char estado2;
 char codigo2[4];
+char cidade2[50];
 float populacao2;
 float area2;
 float pib2;
 int pontos_turisticos2;
+float densidade_populacional2;
+float pib_por_capita2;
 
   // Área para entrada de dados
   // PRIMEIRA CARTA
 printf("PRIMEIRA CARTA\n");
 
-printf("informe o estado:\n");
-scanf(" %9s", estado1);
+printf("Informe a Cidade:\n");
+scanf(" %49s", cidade1);
 
-printf("informe o codigo:\n");
+printf("Informe o Estado:\n");
+scanf(" %c", &estado1);
+
+printf("Informe o Codigo:\n");
 scanf(" %3s", codigo1);
 
-printf("informe a população:\n");
+printf("Informe a População:\n");
 scanf("%f", &populacao1);
 
-printf("informe a area:\n");
+printf("Informe a Area:\n");
 scanf("%f", &area1);
 
-printf("informe o PIB:\n");
+printf("Informe o PIB:\n");
 scanf("%f", &pib1);
 
-printf("informe quantos pontos turisticos:\n");
+printf("Informe Quantos Pontos Turisticos:\n");
 scanf("%d", &pontos_turisticos1);
 
   // SEGUNDA CARTA
 printf("SEGUNDA CARTA\n");
 
-printf("informe o estado:\n");
-scanf(" %9s", estado2);
+printf("Informe a Cidade:\n");
+scanf(" %49s", cidade2);
 
-printf("informe o codigo:\n");
+printf("Informe o Estado:\n");
+scanf(" %c", &estado2);
+
+printf("Informe o Codigo:\n");
 scanf(" %3s", codigo2);
 
-printf("informe a população:\n");
+printf("Informe a População:\n");
 scanf("%f", &populacao2);
 
-printf("informe a area:\n");
+printf("Informe a Area:\n");
 scanf("%f", &area2);
 
-printf("informe o PIB:\n");
+printf("Informe o PIB:\n");
 scanf("%f", &pib2);
 
-printf("informe quantos pontos turisticos:\n");
+printf("Informe Quantos Pontos Turisticos:\n");
 scanf("%d", &pontos_turisticos2);
 
+
+//CALCULO DE DENSIDADE E PIB POR CAPITAL//
+densidade_populacional1 = populacao1 / area1;
+pib_por_capita1 = pib1 / populacao1;
+
+densidade_populacional2 = populacao2 / area2;
+pib_por_capita2 = pib2 / populacao2;
 
 
   // Área para exibição dos dados da cidade
 printf("PRIMEIRA CARTA COMPOSTA POR:\n");
-printf("estado: %s\n", estado1);
-printf("codigo: %s\n", codigo1);
-printf("população: %.2f\n", populacao1);
-printf("area: %.2f\n", area1);
+printf("Estado: %c\n", estado1);
+printf("Codigo: %s\n", codigo1);
+printf("População: %.2f\n", populacao1);
+printf("Area: %.2f\n", area1);
 printf("PIB: %.2f\n", pib1);
-printf("pontos turisticos: %d\n", pontos_turisticos1);
+printf("Pontos Turisticos: %d\n", pontos_turisticos1);
+printf("Densidade Populacional: %.2f\n", densidade_populacional1);
+printf("PIB por capita: %.2f\n", pib_por_capita1);
+
 
 printf("SEGUNDA CARTA COMPOSTA POR:\n");
-printf("estado: %s\n", estado2);
-printf("codigo: %s\n", codigo2);
-printf("população: %.2f\n", populacao2);
-printf("area: %.2f\n", area2);
+printf("Estado: %c\n", estado2);
+printf("Codigo: %s\n", codigo2);
+printf("População: %.2f\n", populacao2);
+printf("Area: %.2f\n", area2);
 printf("PIB: %.2f\n", pib2);
-printf("pontos turisticos: %d\n", pontos_turisticos2);
+printf("Pontos Turisticos: %d\n", pontos_turisticos2);
+printf("Densidade Populacional: %.2f\n", densidade_populacional2);
+printf("PIB por capita: %.2f\n", pib_por_capita2);
+
 
 return 0;
 
